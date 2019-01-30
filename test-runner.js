7553e6e2fe7a5f27156168a40837f55ba8e4806f
R Sánchez <rene.as.himself@gmail.com>
2019-01-30 13:48:15 -0600

    Revert "Code cleanup"
    This reverts commit 27da94cd959698a999f4bf9d24762c928f5128db.

--- test-runner.js
+++ test-runner.js
@@ -1,63 +1,65 @@
-const analyser = require('./assertion-analyser')
-const EventEmitter = require('events').EventEmitter
+var analyser = require('./assertion-analyser');
+var EventEmitter = require('events').EventEmitter;
 
-const Mocha = require('mocha'),
+var Mocha = require('mocha'),
     fs = require('fs'),
-    path = require('path')
+    path = require('path');
 
-const mocha = new Mocha()
-const testDir = './tests'
+var mocha = new Mocha();
+var testDir = './tests'
+
 
 // Add each .js file to the mocha instance
-fs.readdirSync(testDir)
-    .filter(function(file) {
-        // Only keep the .js files
-        return file.substr(-3) === '.js'
-    })
-    .forEach(function(file) {
-        mocha.addFile(path.join(testDir, file))
-    })
+fs.readdirSync(testDir).filter(function(file){
+    // Only keep the .js files
+    return file.substr(-3) === '.js';
 
-const emitter = new EventEmitter()
+}).forEach(function(file){
+    mocha.addFile(
+        path.join(testDir, file)
+    );
+});
+
+var emitter = new EventEmitter();  
 emitter.run = function() {
-    const tests = []
-    const context = ''
-    const separator = ' -> '
-    // Run the tests.
-    try {
-        const runner = mocha
-            .ui('tdd')
-            .run()
-            .on('test end', function(test) {
-                // remove comments
-                const body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '')
-                // collapse spaces
-                body = body.replace(/\s+/g, ' ')
-                const obj = {
-                    title: test.title,
-                    context: context.slice(0, -separator.length),
-                    state: test.state,
-                    // body: body,
-                    assertions: analyser(body),
-                }
-                tests.push(obj)
-            })
-            .on('end', function() {
-                emitter.report = tests
-                emitter.emit('done', tests)
-            })
-            .on('suite', function(s) {
-                context += s.title + separator
-            })
-            .on('suite end', function(s) {
-                context = context.slice(0, -(s.title.length + separator.length))
-            })
-    } catch (e) {
-        throw e
-    }
-}
 
-module.exports = emitter
+  var tests = [];
+  var context = "";
+  var separator = ' -> ';
+  // Run the tests.
+  try {
+  var runner = mocha.ui('tdd').run()
+    .on('test end', function(test) {
+        // remove comments
+        var body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');
+        // collapse spaces
+        body = body.replace(/\s+/g,' ');
+        var obj = {
+          title: test.title,
+          context: context.slice(0, -separator.length),
+          state: test.state,
+          // body: body,
+          assertions: analyser(body)
+        };
+        tests.push(obj);
+    })
+    .on('end', function() {
+        emitter.report = tests;
+        emitter.emit('done', tests)
+    })
+    .on('suite', function(s) {
+      context += (s.title + separator);
+
+    })
+    .on('suite end', function(s) {
+      context = context.slice(0, -(s.title.length + separator.length))
+    })
+  } catch(e) {
+    throw(e);
+  }
+};
+
+module.exports = emitter;
 
 /*
  * Mocha.runner Events:
